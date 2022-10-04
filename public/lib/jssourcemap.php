479f3b94a400479dc18fc5c0971836fc2a6070dc
Andrew Nicols <andrew@nicols.co.uk>
2022-10-04 09:51:48 +0800

    MDL-66107 js: Stop bundling sourceContent in JS Maps
    The commit removes the `sourceContent` from the generated map files
    during run-time as this is no longer required.
    
    When we first landed our Babel transpilation for Moodle, we attempted to
    include combined map files, whereby the combined first.js file had a
    single sourceMap referencing all of the source files.
    
    This worked on Chrome, but did not work in Firefox due to a bug with
    their sourceMap support.
    
    See https://bugzilla.mozilla.org/show_bug.cgi?id=1556903 for more
    information that bug.
    
    As a compromise we only put sourceMaps into development code, and we
    included the sourceContent within the generated map. This allowed us to
    work around that bug entirely and improve the development experience in
    Firefox.
    
    That issue how now been resolved for some time, and all affected
    versions of Firefox are long out of use and support. This means that we
    no longer need to include the sourceContent in the generated sourcemap.
    
    Although it is possible to remove this in the build process too, doing
    so will lead to changes in build output which will affect developers. We
    should avoid making this change until we need to make other changes to
    the build output and make these changes all at once.
    
    By removing the sourceContent from the generated sourcemap at runtime,
    we can at least reduce over-the-wire size, which is still beneficial to
    developers.

--- public/lib/jssourcemap.php
+++ public/lib/jssourcemap.php
@@ -38,20 +38,16 @@ if (!$slashargument) {
 }
 
 $slashargument = ltrim($slashargument, '/');
-// Split into revision and module name.
-[$file] = explode('/', $slashargument, 1);
-$file = '/' . min_clean_param($file, 'SAFEPATH');
+$file = '/' . min_clean_param($slashargument, 'SAFEPATH');
 
 // Only load js files from the js modules folder from the components.
 [$unused, $component, $module] = explode('/', $file, 3);
 
-// When running a lazy load, we only deal with one file so we can just return the working sourcemap.
 $jsfiles = core_requirejs::find_one_amd_module($component, $module);
 $jsfile = reset($jsfiles);
 $shortfilename = str_replace($CFG->dirroot, '', $jsfile);
 
-$mapfile = $jsfile . '.map';
-
+$mapfile = "{$jsfile}.map";
 if (file_exists($mapfile)) {
     $mapdata = file_get_contents($mapfile);
     $mapdata = json_decode($mapdata, true);
@@ -60,6 +56,7 @@ if (file_exists($mapfile)) {
     $srcfilename = str_replace('.min.js', '.js', $srcfilename);
     $fullsrcfilename = $CFG->wwwroot . $srcfilename;
     $mapdata['sources'][0] = $fullsrcfilename;
+    unset($mapdata['sourcesContent']);
 
     echo json_encode($mapdata);
 } else {
