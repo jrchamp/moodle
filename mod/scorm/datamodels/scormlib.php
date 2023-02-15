a384a58fb62035d27886336eb387ad1d0b2ae7ed
Jonathan Champ <jrchamp@ncsu.edu>
2023-02-15 17:54:40 -0500

    php 8 friendly checks

--- mod/scorm/datamodels/scormlib.php
+++ mod/scorm/datamodels/scormlib.php
@@ -45,7 +45,7 @@ function scorm_get_manifest($blocks, $scoes) {
     static $organization;
 
     $manifestresourcesnotfound = array();
-    if (count($blocks) > 0) {
+    if (!empty($blocks) && is_iterable($blocks)) {
         foreach ($blocks as $block) {
             switch ($block['name']) {
                 case 'METADATA':
@@ -568,7 +568,7 @@ function scorm_parse_scorm(&$scorm, $manifest) {
     $scoes = scorm_get_manifest($manifests, $scoes);
     $newscoes = array();
     $sortorder = 0;
-    if (count($scoes->elements) > 0) {
+    if (!empty($scoes->elements) && is_iterable($scoes->elements)) {
         $olditems = $DB->get_records('scorm_scoes', array('scorm' => $scorm->id));
         foreach ($scoes->elements as $manifest => $organizations) {
             foreach ($organizations as $organization => $items) {
