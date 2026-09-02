725f53bbd1b9331080211ed8ae427de40d390ee1
Jonathan Champ <jonathan.r.champ@gmail.com>
2026-09-02 16:54:43 +0200

    MDL-89723 core: PHP 8.5 setlocale needs string '0'

--- public/lib/classes/test/phpunit/phpunit_util.php
+++ public/lib/classes/test/phpunit/phpunit_util.php
@@ -164,7 +164,7 @@ class phpunit_util extends \core\test\testing_util {
                 $warnings[] = 'Warning: unexpected change of $FULLME';
             }
 
-            if (setlocale(LC_TIME, 0) !== $localename) {
+            if (setlocale(LC_TIME, '0') !== $localename) {
                 $warnings[] = 'Warning: unexpected change of locale';
             }
         }
